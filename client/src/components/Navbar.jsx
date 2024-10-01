0d47733307ce069d4f8485d1eb6296b6c89b1962
stmcpeters <stmcpeters@gmail.com>
2024-09-30 20:36:01 -0700

    deletes unused template items

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import Logo from '../assets/BlueTechtonicaWord.png'
 
 
 function MyNavBar(props) {
@@ -11,18 +10,18 @@ function MyNavBar(props) {
     <Navbar bg="dark" variant="dark" sticky="top">
       <Container>
         <Navbar.Brand href="/">
-        <img
+        {/* <img
               src={Logo}
               height="30"
               className="d-lg-inline-block"
               alt="React Bootstrap logo"
-            />
+            /> */}
         </Navbar.Brand>
-        <Nav.Link >Your Link</Nav.Link>
+        {/* <Nav.Link >Your Link</Nav.Link> */}
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
           <Navbar.Text>
-            Signed in as: <a href="#login">Cristina Rodriguez</a>
+            {/* Signed in as: <a href="#login">Cristina Rodriguez</a> */}
           </Navbar.Text>
         </Navbar.Collapse>
       </Container>
